f66f699fbee0f0b972c9d0f693bb86bf7c57638e
Mark Schloeman <markofwisdumb@gmail.com>
2020-09-06 12:55:16 -0400

    I am trying to use Polymorphism to make this code easy to read and easy to updated. So far I think it's working.
    I made two new base classes - ReminderOptions and Reminder
    
    ReminderOptions are QWidgets that will be used to get arbitrary input
    from the user. They all need to implement a method called get_reminder
    
    get_reminder creates a new Reminder object with arguments taken from the
    ReminderOptions widget.
    
    The Reminder class must implement the handle method. The handle method
    is called by the main program and should cause something to happend to
    remind the user to take a break.
    
    Currently have two reminder types - BrowserReminder and
    RaiseWindowReminder. They each have their own ReminderOptions -
    BrowserReminderOptions and RaiseWindowReminderOptions.
    
    Example on how the Reminder and ReminderOptions work:
    The BrowserReminder has two unique settings the user can set
      - URL: which website they want to open at each reminder
      - New Tab Policy: this is the 'new' parameter in the webbrowser.open
      method. It determines whether you open the URL in a new window, new
      tab, or reload the existing window.
    The BorwserReminderOptions provides a QLineEdit to enter the URL and
    lets you choose the new tab policy with QRadioButtons.
    
    BroserReminderOptions.get_reminder() returns
    BrowserReminder(url=QLineEdit.text() and new_tab_policy=QRadioButton)
    BrowserReminder.handle() executes webbrowser.open(url, new_tab_policy)
    
    I' famished and have to go to work soon so I sort of rushed to type this
    so I don't expect it to be very coherent. Sorry future self.

--- standup.py
+++ standup.py
@@ -6,7 +6,7 @@
 
 import webbrowser
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QLabel, QLineEdit, QGridLayout, QComboBox, QPushButton, QVBoxLayout, QSpinBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QLabel, QLineEdit, QGridLayout, QComboBox, QPushButton, QVBoxLayout, QSpinBox, QFormLayout, QButtonGroup, QGroupBox, QHBoxLayout, QRadioButton
 from PyQt5.QtCore import QTimer, QTime, QSettings, Qt
 
 
@@ -24,6 +24,14 @@ class StandUp(QMainWindow):
 
     def init_ui(self):
         self.stack = QStackedWidget()
+
+        self.reminder_screen = QWidget()
+        reminder_vbox = QVBoxLayout(self.reminder_screen)
+        self.reminder_message = QLabel()
+        close_reminder = QPushButton("Close Reminder")
+        close_reminder.clicked.connect(self.close_reminder)
+        reminder_vbox.addWidget(self.reminder_message)
+        reminder_vbox.addWidget(close_reminder)
 
         self.start_screen = QWidget()
         self.start_grid = QGridLayout(self.start_screen)
@@ -46,32 +54,12 @@ class StandUp(QMainWindow):
         self.interval_entry.setSuffix(" minutes")
 
         reminder_type_label = QLabel("Reminder Type:")
-        # NOTE TODO NOTE TODO
-        # change the signal and slots for these
         self.reminder_select = QComboBox()
-        self.reminder_select.currentTextChanged.connect(self.show_reminder_type_options)
-
         self.reminder_type_options = QStackedWidget()
-        self.url_options = QWidget()
-        url_options_grid = QGridLayout(self.url_options)
+        self.reminder_select.currentIndexChanged.connect(self.reminder_type_options.setCurrentIndex)
 
-        url_label = QLabel("URL:")
-        self.url_entry = QLineEdit()
-        # TODO add a QGroupBox that lets the user choose how to open the URL
-        #      new tab or new browser?
-        #self.url_open_policy = QGroupBox()
-
-        url_options_grid.addWidget(url_label, 0, 0)
-        url_options_grid.addWidget(self.url_entry, 0, 1)
-
-        self.app_reminder_options = QWidget()
-        app_reminder_grid = QGridLayout(self.app_reminder_options)
-
-        reminder_message_label = QLabel("Reminder Message:")
-        self.reminder_text = QLineEdit()
-
-        app_reminder_grid.addWidget(reminder_message_label, 0, 0)
-        app_reminder_grid.addWidget(self.reminder_text, 0, 1)
+        self.url_options = BrowserReminderOptions()
+        self.app_reminder_options = RaiseWindowReminderOptions(self.stack, self.reminder_screen, self.reminder_message)
 
         self.reminder_type_options.addWidget(self.url_options)
         self.reminder_type_options.addWidget(self.app_reminder_options)
@@ -110,14 +98,6 @@ class StandUp(QMainWindow):
         self.running_vbox.addWidget(stop_button)
         self.running_vbox.setAlignment(Qt.AlignCenter)
 
-        self.reminder_screen = QWidget()
-        reminder_vbox = QVBoxLayout(self.reminder_screen)
-        self.reminder_message = QLabel()
-        close_reminder = QPushButton("Close Reminder")
-        close_reminder.clicked.connect(self.close_reminder)
-        reminder_vbox.addWidget(self.reminder_message)
-        reminder_vbox.addWidget(close_reminder)
-
         self.stack.addWidget(self.start_screen)
         self.stack.addWidget(self.running_screen)
         self.stack.addWidget(self.reminder_screen)
@@ -127,6 +107,7 @@ class StandUp(QMainWindow):
     def init_settings(self):
         pass
 
+    # NOTE if the changes I make now work, delete this
     def show_reminder_type_options(self, reminder_type):
         if reminder_type == 'Open URL':
             self.reminder_type = 'URL'
@@ -135,6 +116,7 @@ class StandUp(QMainWindow):
             self.reminder_type = 'FOCUS'
             self.reminder_type_options.setCurrentWidget(self.app_reminder_options)
 
+    # NOTE i will probably rewrite this because it does not let you view the time remaining
     def start_session(self):
         self.duration = self.duration_entry.value()
         self.interval = self.interval_entry.value()
@@ -172,12 +154,108 @@ class StandUp(QMainWindow):
 
     def close_reminder(self):
         if self.timer.isActive():
-            next_break = QTime.currentTime().addSecs(self.interval * 60)
-            self.next_break.setText(f"Next break at: {next_break.toString('h:m p')}")
+            time_left = QTime.currentTime().addSecs(self.interval * 60)
+            self.next_break.setText(f"Next break at: {time_left.toString('h:m p')}")
             self.stack.setCurrentWidget(self.running_screen)
         else:
             self.stack.setCurrentWidget(self.start_screen)
 
+class ReminderOptions(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_reminder(self):
+        raise NotImplementedError
+
+class BrowserReminderOptions(ReminderOptions):
+
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        form = QFormLayout(self)
+
+        url_label = QLabel("URL:")
+        self.url_entry = QLineEdit()
+
+        tab_policies = QGroupBox("Open URL In...")
+        self.policy_group = QButtonGroup()
+        tab_policies.setFlat(True)
+        hbox = QHBoxLayout(tab_policies)
+        same_window = QRadioButton("Reload Window")
+        new_window = QRadioButton("New Browser Window")
+        new_tab = QRadioButton("New Tab")
+
+        self.policy_group.addButton(same_window, id=0)
+        self.policy_group.addButton(new_window, id=1)
+        self.policy_group.addButton(new_tab, id=2)
+        hbox.addWidget(same_window)
+        hbox.addWidget(new_window)
+        hbox.addWidget(new_tab)
+
+        form.addRow(url_label, self.url_entry)
+        form.addRow(tab_policies)
+
+    def get_reminder(self):
+        return BrowserReminder(self.url_entry.text(), self.policy_group.checkedId())
+
+class RaiseWindowReminderOptions(ReminderOptions):
+
+    def __init__(self, parent_stack, reminder_widget, message_widget):
+        super().__init__()
+
+        self.parent_stack = parent_stack
+        self.reminder_widget = reminder_widget
+        self.message_widget = message_widget
+
+        self.init_ui()
+
+    def init_ui(self):
+        form = QFormLayout(self)
+
+        reminder_message_label = QLabel("Reminder Message:")
+        self.reminder_input = QLineEdit()
+
+        form.addRow(reminder_message_label, self.reminder_input)
+
+    def set_reminder_text(self):
+        self.message_widget.setText(self.reminder_input.text())
+
+    def get_reminder(self):
+        self.set_reminder_text()
+        return RaiseWindowReminder(self.parent_stack, self.reminder_widget)
+
+
+class Reminder:
+    def __init__(self):
+        super().__init__()
+
+    def handle(self):
+        raise NotImplementedError
+
+class BrowserReminder(Reminder):
+
+    def __init__(self, url, new_tab_policy=2):
+        super().__init__()
+        self.url = url
+        self.new_tab_policy = new_tab_policy
+
+    def handle(self):
+        webbrowser.open(self.url, new=self.new_tab_policy)
+
+class RaiseWindowReminder(Reminder):
+
+    def __init__(self, parent_stack, reminder_widget):
+        super().__init__()
+        self.parent_stack = parent_stack
+        self.reminder_widget = reminder_widget
+
+    def handle(self):
+        self.parent_stack.setCurrentWidget(self.reminder_widget)
+        self.reminder_widget.activateWindow()
 
 
 def main():
